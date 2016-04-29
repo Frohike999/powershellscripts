21b52fd2ba2e1d1dc2cbb9a2019092f70ca090e7
Frohike999 <frohos@gmail.com>
2016-04-29 13:52:35 -0400

    Update execute-sql-all-instances.ps1

--- execute-sql-all-instances.ps1
+++ execute-sql-all-instances.ps1
@@ -1,4 +1,4 @@
-//Applies SQL script to all SQL instances on a computer. In the command below, SYSTEM is given sysadmin role for each instance.
+<# Applies SQL script to all SQL instances on a computer. In the command below, SYSTEM is given sysadmin role for each instance. #>
 
 $i = (get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances | ForEach-Object { "localhost\$_" }
 
