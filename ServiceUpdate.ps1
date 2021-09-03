a385892af5a25ae3ce8ffc219e93b6ac37057350
Scott Brown <frohos@gmail.com>
2021-09-03 14:53:54 -0400

    Add files via upload
    This gets service names passed as parameter and sends an email if any aren't running

---
+++ ServiceUpdate.ps1
@@ -0,0 +1,34 @@
+﻿$ComputerName = (Get-WmiObject Win32_Computersystem).name
+$ServiceName = $args[0]
+$ServiceDisplayName = (Get-Service $ServiceName).DisplayName
+$TimesRestarted = $args[1]
+
+$Status = (Get-Service $ServiceName).Status
+
+if ($Status -ne "Running")
+{
+    Start-Service $ServiceName
+}
+
+function SendAlert
+{
+    $FromAddress = "HostedServiceFailure@xanatek.com"
+    $ToAddress = "scott@xanatek.com"
+    $MessageSubject = "Hosted Service Failure for $ComputerName"
+    $MessageBody = "The $ServiceDisplayName ($ServiceName) service on $ComputerName has restarted $TimesRestarted times in the last 24 hours, please investigate immediately."
+    $SendingServer = "mail.xanatek.net"
+    
+    $EmailUsername = "HostedServiceFailure"
+    $encrypted = Get-Content "C:\ims\email.txt" | ConvertTo-SecureString
+    $EmailCredential = New-Object System.Management.Automation.PSCredential($EmailUsername, $encrypted)
+
+    $SMTPMessage = New-Object System.Net.Mail.MailMessage $FromAddress, $ToAddress, $MessageSubject, $MessageBody
+
+    $SMTPClient = New-Object System.Net.Mail.SmtpClient $SendingServer
+    $SMTPClient.Credentials = $EmailCredential
+    $SMTPClient.Send($SMTPMessage)
+}
+
+SendAlert
+
+
